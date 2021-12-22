292f284cf50b682cde49c42f8f7562e2c300c113
Angeloamenta <angeloamenta7@gmail.com>
2021-12-22 16:03:13 +0100

    fix delete

--- js/script.js
+++ js/script.js
@@ -3,190 +3,190 @@
 // Click sul contatto mostra la conversazione del contatto cliccato
 
 var app = new Vue({
-    el: '#app',
-    data: {
-      contattotemporaneo : [],
-      menu: false,
-      searchcontact: "",
-      messtype: "",
-      message: "",
-      dates:"",
-      lastmessage:"",
-      cont:"0",
-      answer: "answer",
-      
-      contacts: [
-        {
-          name: "Michele",
-          avatar: "_1",
-          visible: true,
-          messages: [
-            {
-              date: "10/01/2020 15:30:55",
-              text: "Hai portato a spasso il cane?",
-              status: "sent",
-            },
-            {
-              date: "10/01/2020 15:50:00",
-              text: "Ricordati di dargli da mangiare",
-              status: "sent",
-            },
-            {
-              date: "10/01/2020 16:15:22",
-              text: "Tutto fatto!",
-              status: "received",
-            },
-          ],
-        },
-        {
-          name: "Fabio",
-          avatar: "_2",
-          visible: true,
-          messages: [
-            {
-              date: "20/03/2020 16:30:00",
-              text: "Ciao come stai?",
-              status: "sent",
-            },
-            {
-              date: "20/03/2020 16:30:55",
-              text: "Bene grazie! Stasera ci vediamo?",
-              status: "received",
-            },
-            {
-              date: "20/03/2020 16:35:00",
-              text: "Mi piacerebbe ma devo andare a fare la spesa.",
-              status: "sent",
-            },
-          ],
-        },
-      
-        {
-          name: "Samuele",
-          avatar: "_3",
-          visible: true,
-          messages: [
-            {
-              date: "28/03/2020 10:10:40",
-              text: "La Marianna va in campagna",
-              status: "received",
-            },
-            {
-              date: "28/03/2020 10:20:10",
-              text: "Sicuro di non aver sbagliato chat?",
-              status: "sent",
-            },
-            {
-              date: "28/03/2020 16:15:22",
-              text: "Ah scusa!",
-              status: "received",
-            },
-          ],
-        },
-        {
-          name: "Luisa",
-          avatar: "_4",
-          visible: true,
-          messages: [
-            {
-              date: "10/01/2020 15:30:55",
-              text: "Lo sai che ha aperto una nuova pizzeria?",
-              status: "sent",
-            },
-            {
-              date: "10/01/2020 15:50:00",
-              text: "Si, ma preferirei andare al cinema",
-              status: "received",
-            },
-          ],
-        },
-      ]
-    },
-    methods : {
-
-      select : function (index){
-        this.cont = index;
-        console.log(this.contacts[this.cont].visible);
-        // this.contacts[this.cont].visible = false
-        console.log("cont",this.contacts[this.cont]);
+  el: '#app',
+  data: {
+    contattotemporaneo : [],
+    menu: false,
+    searchcontact: "",
+    messtype: "",
+    message: "",
+    dates:"",
+    lastmessage:"",
+    cont:"0",
+    answer: "answer",
+    
+    contacts: [
+      {
+        name: "Michele",
+        avatar: "_1",
+        visible: true,
+        messages: [
+          {
+            date: "10/01/2020 15:30:55",
+            text: "Hai portato a spasso il cane?",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 15:50:00",
+            text: "Ricordati di dargli da mangiare",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 16:15:22",
+            text: "Tutto fatto!",
+            status: "received",
+          },
+        ],
       },
+      {
+        name: "Fabio",
+        avatar: "_2",
+        visible: true,
+        messages: [
+          {
+            date: "20/03/2020 16:30:00",
+            text: "Ciao come stai?",
+            status: "sent",
+          },
+          {
+            date: "20/03/2020 16:30:55",
+            text: "Bene grazie! Stasera ci vediamo?",
+            status: "received",
+          },
+          {
+            date: "20/03/2020 16:35:00",
+            text: "Mi piacerebbe ma devo andare a fare la spesa.",
+            status: "sent",
+          },
+        ],
+      },
+    
+      {
+        name: "Samuele",
+        avatar: "_3",
+        visible: true,
+        messages: [
+          {
+            date: "28/03/2020 10:10:40",
+            text: "La Marianna va in campagna",
+            status: "received",
+          },
+          {
+            date: "28/03/2020 10:20:10",
+            text: "Sicuro di non aver sbagliato chat?",
+            status: "sent",
+          },
+          {
+            date: "28/03/2020 16:15:22",
+            text: "Ah scusa!",
+            status: "received",
+          },
+        ],
+      },
+      {
+        name: "Luisa",
+        avatar: "_4",
+        visible: true,
+        messages: [
+          {
+            date: "10/01/2020 15:30:55",
+            text: "Lo sai che ha aperto una nuova pizzeria?",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 15:50:00",
+            text: "Si, ma preferirei andare al cinema",
+            status: "received",
+          },
+        ],
+      },
+    ]
+  },
+  methods : {
 
-    invio : function () {
-      console.log(this.message);
-      let testomex = this.contacts[this.cont].messages;
-      let today = new Date();
-
-        let date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-
-        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-        let dateTime = date+' '+time;
-
-      let obj = {
-        date: dateTime,
-              text: this.message,
-              status: "sent",
-      }
-
-      testomex.push(obj)
-
-        if (this.message.length > 0) {
-          this.timer = setTimeout(function() {
-          let objt = {
-
-            date: dateTime,
-                  text: "si, ok",
-                  status: "recived",
-          }
-          testomex.push(objt)
-        }, 3000);
-        }
-      
-        this.message= ""
+    select : function (index){
+      this.cont = index;
+      console.log(this.contacts[this.cont].visible);
+      // this.contacts[this.cont].visible = false
+      console.log("cont",this.contacts[this.cont]);
     },
 
-    deleteThis : function (index) {
-      console.log("ciao");
-      this.contacts[this.cont].messages.splice(index);
-    },
-    
-    showMenu : function (index) {
-      console.log(index);
-      console.log("vero-falso", this.contacts[index].visible);
+  invio : function () {
+    console.log(this.message);
+    let testomex = this.contacts[this.cont].messages;
+    let today = new Date();
 
-      // if (this.contacts[index].visible = true) {
+      let date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
 
-      //   this.contacts[index].visible = false
-        
-      // }else if (this.contacts[index].visible = false) {
-      //   console.log("cancello");
-      //   this.contacts[index].visible = true
-      // }
-      this.contacts[index].visible = !this.contacts[index].visible
-    },
-    
+      let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
-    // funzione creta inutilmente -.-
-    // filtrocontatti : function (){
-    //  let contattotemporaneo = this.contacts.filter((contatto) => { 
-    //     if (contatto.name = this.searchcontact) {
-    //       return true;
-    //     } else {
-    //       return false;
-    //     }
-    //   })
-    //   console.log(contattotemporaneo);
-    // }
-     },
+      let dateTime = date+' '+time;
 
-    
-    created() {
-      
-
-      
-      
-      
-     
+    let obj = {
+      date: dateTime,
+            text: this.message,
+            status: "sent",
     }
+
+    testomex.push(obj)
+
+      if (this.message.length > 0) {
+        this.timer = setTimeout(function() {
+        let objt = {
+
+          date: dateTime,
+                text: "si, ok",
+                status: "recived",
+        }
+        testomex.push(objt)
+      }, 3000);
+      }
     
-  })
+      this.message= ""
+  },
+
+  deleteThis : function (index) {
+    console.log("ciao");
+    this.contacts[this.cont].messages.splice(index, 1);
+  },
+  
+  showMenu : function (index) {
+    console.log(index);
+    console.log("vero-falso", this.contacts[index].visible);
+
+    // if (this.contacts[index].visible = true) {
+
+    //   this.contacts[index].visible = false
+      
+    // }else if (this.contacts[index].visible = false) {
+    //   console.log("cancello");
+    //   this.contacts[index].visible = true
+    // }
+    this.contacts[index].visible = !this.contacts[index].visible
+  },
+  
+
+  // funzione creta inutilmente -.-
+  // filtrocontatti : function (){
+  //  let contattotemporaneo = this.contacts.filter((contatto) => { 
+  //     if (contatto.name = this.searchcontact) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   })
+  //   console.log(contattotemporaneo);
+  // }
+   },
+
+  
+  created() {
+    
+
+    
+    
+    
+   
+  }
+  
+})
